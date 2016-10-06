e9b69b6e40dc9dc57b46ddec72e8df682a408c21
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-10-06 12:38:09 -0300

    adding ci

--- dir_glob.rb
+++ dir_glob.rb
@@ -4,13 +4,12 @@ require 'fileutils'
 FileUtils.rm('Manifest')
 
 def glob(path)
-	return if !File.directory?(path) && path != "**"
-	path = "#{path}/*"
-	Dir.glob(path) do |file|
-		File.open("Manifest", 'a') {|f| f.write("#{file}\n") }
-		glob(file)
-	end
+  return if !File.directory?(path) && path != '**'
+  path = "#{path}/*"
+  Dir.glob(path) do |file|
+    File.open('Manifest', 'a') { |f| f.write("#{file}\n") }
+    glob(file)
+  end
 end
 
-glob("**")
-
+glob('**')
